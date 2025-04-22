5e21613764eed6043210d79b0dd35096c38d4b80
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-04-22 15:04:39 -0500

    Import App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Accordion from "./components/Accordion/Accordion";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <Accordion />
+    </>
+  );
+}

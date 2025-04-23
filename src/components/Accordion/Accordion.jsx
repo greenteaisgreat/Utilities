eccee0745cfbe87cf9e55ffd20634a231cb3cdb7
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-04-23 09:07:50 -0500

    Add multiselection for Accordion component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -4,27 +4,49 @@ import "./accordion.css";
 
 export default function Accordion() {
   const [selectedId, setSelectedId] = React.useState(null);
+  const [isMultiSelected, setIsMultiSelected] = React.useState(false);
+  const [multiple, setMultiple] = React.useState({});
 
   function handleSelection(currentId) {
     setSelectedId(currentId === selectedId ? null : currentId);
   }
 
+  function handleMultiSelection(multiId) {
+    const newMultiple = { ...multiple };
+
+    if (newMultiple[multiId]) newMultiple[multiId] = false;
+    else newMultiple[multiId] = true;
+
+    setMultiple(newMultiple);
+  }
+
   return (
     <div className="wrapper">
+      <button onClick={() => setIsMultiSelected(!isMultiSelected)}>
+        {isMultiSelected ? "Disable" : "Enable"} multi-selection
+      </button>
       <div className="accordion">
         {data.length > 0 ? (
           data.map((item, i) => (
             <div className="item" key={i}>
               <div
                 className="question"
-                onClick={() => handleSelection(item.id)}
+                onClick={() =>
+                  isMultiSelected
+                    ? handleMultiSelection(item.id)
+                    : handleSelection(item.id)
+                }
               >
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
-              {selectedId === item.id && (
-                <div className="answer">{item.answer}</div>
-              )}
+              {isMultiSelected
+                ? multiple[item.id] && (
+                    <div className="answer">{item.answer}</div>
+                  )
+                : selectedId === item.id && (
+                    <div className="answer">{item.answer}</div>
+                  )}
             </div>
           ))
         ) : (

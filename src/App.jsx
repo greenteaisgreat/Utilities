d9306ca34b398719b9c689894c252f7a3baa2948
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-05-12 10:30:18 -0500

    Import <ColorGenerator />

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Accordion from "./components/Accordion/Accordion";
+import GenerateColor from "./components/GenerateColor/GenerateColor";
 import "./App.css";
 
 export default function App() {
   return (
     <>
       <Accordion />
+      <GenerateColor />
     </>
   );
 }

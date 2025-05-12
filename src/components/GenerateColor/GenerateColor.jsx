7ee605d8d17a19768971d400e3cd490ad9101a1c
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-05-12 12:58:07 -0500

    Add generative logic for hex colors

--- src/components/GenerateColor/GenerateColor.jsx
+++ src/components/GenerateColor/GenerateColor.jsx
@@ -6,12 +6,42 @@ export default function GenerateColor() {
   const [colorType, setColorType] = React.useState("hex");
   const [colorCode, setColorCode] = React.useState("#000");
 
+  function handleGenerateRandomColor() {
+    if (colorType === "hex") {
+      let hexColor = "#";
+      const hexArr = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+      ];
+      for (let i = 0; i < 6; i++) {
+        hexColor += hexArr[Math.floor(Math.random() * hexArr.length)];
+      }
+      setColorCode(hexColor);
+    }
+  }
+
   return (
-    <div className="container">
+    <div className="container" style={{ "--colorCode": colorCode }}>
       <div className="button-wrapper">
-        <button>Generate HEX Color</button>
-        <button>Generate RGB Color</button>
-        <button>Generate Random Color</button>
+        <button onClick={() => setColorType("hex")}>Generate HEX Color</button>
+        <button onClick={() => setColorType("rgb")}>Generate RGB Color</button>
+        <button onClick={handleGenerateRandomColor}>
+          Generate Random Color
+        </button>
       </div>
     </div>
   );

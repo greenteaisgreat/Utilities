e2b4c4c7c13fcfc798a1083af5daa41738361b59
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-05-27 16:00:26 -0500

    Add fetching functionality for ImageSlider

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import "./imageSlider.css";
 
-export default function ImageSlider() {
+export default function ImageSlider({ url, limit }) {
+  const [images, setImages] = React.useState([]);
+  const [sliderIndex, setSliderIndex] = React.useState(0);
+
+  async function fetchImages(getUrl) {
+    try {
+      const response = await fetch(getUrl);
+      const data = await response.json();
+      console.log(data);
+
+      if (data.ok()) {
+        setImages(data.json);
+      }
+    } catch (error) {
+      console.error(`An error has occurred: ${error}`);
+    }
+  }
+
+  React.useEffect(() => {
+    if (url === "") {
+      console.error("URL must not be blank");
+    }
+    fetchImages(url);
+  }, []);
+
   return <div className="image-slider-wrapper">Test</div>;
 }

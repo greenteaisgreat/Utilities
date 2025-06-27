de33a3e8284e67a9e4abdedfaaa48b0ff5d48103
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-06-27 13:41:34 -0500

    Scaffold LoadMore component

---
+++ src/components/LoadMore/LoadMore.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LoadMore(url, limit = 5) {
+  const [items, setItems] = React.useState([]);
+
+  return (
+    <div className="loader-wrapper">
+      <h1>Test!</h1>
+    </div>
+  );
+}

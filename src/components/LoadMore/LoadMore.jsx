e7475e9d9d5a77101f79afe26f86abdfb6080f47
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2025-06-30 13:29:06 -0500

    Implement basic network request functionality

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 export default function LoadMore(url, limit = 5) {
   const [items, setItems] = React.useState([]);
+  // idle | loading | success | error
+  const [status, setStatus] = React.useState("idle");
+  const [buttonClicks, setButtonClicks] = React.useState(0);
+
+  async function getProducts(url) {
+    try {
+      setStatus("loading");
+      const response = await fetch(url);
+      const json = await response.json();
+
+      if (json) {
+        setStatus("success");
+        setItems([...json]);
+      }
+    } catch (error) {
+      setStatus("error");
+      console.error(`There was an error: ${error}`);
+    }
+  }
 
   return (
     <div className="loader-wrapper">
